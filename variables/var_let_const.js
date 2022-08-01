9b9d910e3428b78ee9d195f3fb6ba6fdb54ac15f
Marion Perez <marion.oceane.perez@gmail.com>
2022-08-01 09:53:32 +0200

    ajout des variables de type let

--- variables/var_let_const.js
+++ variables/var_let_const.js
@@ -8,4 +8,29 @@ console.log("2. Nom : " + nom);
 // de la variable il affiche bien "Marion"
 // Cela est possible car le "moteur" Javascript va créer une enveloppe contenant tous le code pour s'assurer que les normes du langage sont 
 // respectés, avant d'exécuter le code. Cette étape réserve automatiquement de l'espace en mémoire pour toutes les variables déclarées comme
-// "var" et va leur assigner la valeur "undefined"
+// "var" et va leur assigner la valeur "undefined" (c'est ce que l'on appelle le hoisting)
+
+// Mais cela peu poser problème :
+// Variable globale (accès à a=6 même en dehors de la boucle ce que l'on ne veut pas)
+
+if(true) {
+    var a = 6;
+    console.log("'a' dans 'IF'", a);
+}
+console.log("'a' après 'IF'", a);
+
+// Contrairement au mot clé var, le mot clé let est dit block scoped, c'est à dire que la variable n'est accessible que dans le bloc dans 
+// lequel elle est définie. Un bloc c'est ce qui se trouve entre { et }, c'est le cas des boucles, du if mais aussi des fonctions. 
+// Let ne subit pas de hoisting cce qui en fait une variable locale (dans les deux cas suivant cela lève une erreur)
+
+console.log("1. Nom : " + nom2);
+let nom2 = "Marion";
+console.log("2. Nom : " + nom2);
+
+if(true) {
+    let b = 6;
+    console.log("'b' dans 'IF'", b);
+}
+console.log("'b' après 'IF'", b);
+
+// Il n'est donc plus recommandé d'utiliser les variables de type "var"
